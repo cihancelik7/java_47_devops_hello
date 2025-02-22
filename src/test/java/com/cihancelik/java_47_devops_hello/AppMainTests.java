7cff98f96d4c072e08df592f2f1e04aaf10b5fc3
Cihan Celik <cihan.celik7@gmail.com>
2025-02-22 15:56:09 +0300

    DevOps Hello
    Devops Hello

---
+++ src/test/java/com/cihancelik/java_47_devops_hello/AppMainTests.java
@@ -0,0 +1,13 @@
+package com.cihancelik.java_47_devops_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
